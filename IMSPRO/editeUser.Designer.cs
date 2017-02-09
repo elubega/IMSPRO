fe9fe43f4b56f3ccba310ec646452f0dd6251250
elubega <ericlg.l@gmail.com>
2017-02-09 16:55:53 +0300

    Add Edit user functionality

---
+++ IMSPRO/editeUser.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace IMSPRO
+{
+    partial class editeUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(editeUser));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_userID = new System.Windows.Forms.TextBox();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.txt_firstName = new System.Windows.Forms.TextBox();
+            this.txt_lastName = new System.Windows.Forms.TextBox();
+            this.txt_phone = new System.Windows.Forms.TextBox();
+            this.cbm_level = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(91, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(91, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(91, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "First Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(91, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Last Name:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(91, 308);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 22);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Level:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(91, 275);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 22);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Phone:";
+            // 
+            // txt_userID
+            // 
+            this.txt_userID.Location = new System.Drawing.Point(228, 110);
+            this.txt_userID.Name = "txt_userID";
+            this.txt_userID.ReadOnly = true;
+            this.txt_userID.Size = new System.Drawing.Size(75, 22);
+            this.txt_userID.TabIndex = 7;
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(228, 142);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(242, 22);
+            this.txt_username.TabIndex = 8;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(228, 176);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(242, 22);
+            this.txt_password.TabIndex = 9;
+            // 
+            // txt_firstName
+            // 
+            this.txt_firstName.Location = new System.Drawing.Point(228, 208);
+            this.txt_firstName.Name = "txt_firstName";
+            this.txt_firstName.Size = new System.Drawing.Size(242, 22);
+            this.txt_firstName.TabIndex = 10;
+            // 
+            // txt_lastName
+            // 
+            this.txt_lastName.Location = new System.Drawing.Point(228, 242);
+            this.txt_lastName.Name = "txt_lastName";
+            this.txt_lastName.Size = new System.Drawing.Size(242, 22);
+            this.txt_lastName.TabIndex = 11;
+            // 
+            // txt_phone
+            // 
+            this.txt_phone.Location = new System.Drawing.Point(228, 274);
+            this.txt_phone.Name = "txt_phone";
+            this.txt_phone.Size = new System.Drawing.Size(242, 22);
+            this.txt_phone.TabIndex = 12;
+            // 
+            // cbm_level
+            // 
+            this.cbm_level.FormattingEnabled = true;
+            this.cbm_level.Items.AddRange(new object[] {
+            "Branch",
+            "Store",
+            "Manager"});
+            this.cbm_level.Location = new System.Drawing.Point(228, 304);
+            this.cbm_level.Name = "cbm_level";
+            this.cbm_level.Size = new System.Drawing.Size(177, 24);
+            this.cbm_level.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_delete);
+            this.groupBox1.Controls.Add(this.btn_edit);
+            this.groupBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(39, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(542, 385);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Edit User";
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.Location = new System.Drawing.Point(243, 312);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(101, 41);
+            this.btn_edit.TabIndex = 0;
+            this.btn_edit.Text = "Edit";
+            this.btn_edit.UseVisualStyleBackColor = true;
+            this.btn_edit.Click += new System.EventHandler(this.btn_edit_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(350, 312);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(101, 41);
+            this.btn_delete.TabIndex = 1;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            // 
+            // editeUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 455);
+            this.Controls.Add(this.cbm_level);
+            this.Controls.Add(this.txt_phone);
+            this.Controls.Add(this.txt_lastName);
+            this.Controls.Add(this.txt_firstName);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.txt_userID);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "editeUser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Edit User";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_userID;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.TextBox txt_firstName;
+        private System.Windows.Forms.TextBox txt_lastName;
+        private System.Windows.Forms.TextBox txt_phone;
+        private System.Windows.Forms.ComboBox cbm_level;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_edit;
+    }
+}
--- IMSPRO/userManagement.cs
+++ IMSPRO/userManagement.cs
@@ -18,21 +18,70 @@ namespace IMSPRO
             InitializeComponent();
             loadUsers();
         }
+        private void SetConnection()
+        {
+            sql_con = new SQLiteConnection
+                ("Data Source=ismpro_db.sqlite;Version=3;New=False;Compress=True;");
+        }
         //decearing variables for the connection
         private SQLiteConnection sql_con;
         private SQLiteCommand sql_cmd;
         private SQLiteDataAdapter DB;
         private DataSet DS = new DataSet();
         private DataTable DT = new DataTable();
+
+
+        private void clearEntryForm()
+        {
+            txt_userName.Clear();
+            txt_password.Clear();
+            txt_firstName.Clear();
+            txt_lastName.Clear();
+            txt_phone.Clear();
+            cbm_level.SelectedIndex = -1;
+            txt_userName.Focus();
+        }
+
+
         private void btn_add_Click(object sender, EventArgs e)
         {
+            try
+            {
+                SetConnection();
+                sql_con.Open();
+                string query = "Insert into users(userName, password, firstName, lastName, level, dateCreated, phone)";
+                query += "values(@userName,@password,@firstName,@lastName,@level,@dateCreated, @phone)";
 
+            SQLiteCommand myCommand = new SQLiteCommand(query, sql_con);
+            myCommand.Parameters.AddWithValue("@userName", txt_userName.Text);
+            myCommand.Parameters.AddWithValue("@password", txt_password.Text);
+            myCommand.Parameters.AddWithValue("@firstName", txt_firstName.Text);
+            myCommand.Parameters.AddWithValue("@lastName", txt_lastName.Text);
+            myCommand.Parameters.AddWithValue("@level", cbm_level.SelectedItem);
+            myCommand.Parameters.AddWithValue("@dateCreated", DateTime.Now.ToString("yyyy-MM-dd"));
+            myCommand.Parameters.AddWithValue("@phone", txt_phone.Text);
+
+                int qSuccess = myCommand.ExecuteNonQuery();
+
+            sql_con.Close();
+            if (qSuccess == 1)
+                {
+
+                MessageBox.Show("User has been added", "Success.....!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                loadUsers();
+                clearEntryForm();
+
+
+                }
+               
+            }
+           catch(Exception ex)
+            {
+                MessageBox.Show("There is an Error..."+ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
-        private void SetConnection()
-        {
-            sql_con = new SQLiteConnection
-                ("Data Source=ismpro_db.sqlite;Version=3;New=False;Compress=True;");
-        }
+        
         private void loadUsers()
         {
             try
@@ -40,7 +89,7 @@ namespace IMSPRO
                 SetConnection();
                 sql_con.Open();
                 sql_cmd = sql_con.CreateCommand();
-                string CommandText = "select userName AS 'Username', firstName AS 'First Name', lastName AS 'Last Name', level As 'Level', dateCreated As 'Account Open' from users";
+                string CommandText = "select userName AS 'Username', firstName AS 'First Name', lastName AS 'Last Name', phone AS 'Phone No.:', level As 'Level', dateCreated As 'Account Open' from users";
                 DB = new SQLiteDataAdapter(CommandText, sql_con);
                 DS.Reset();
                 DB.Fill(DS);
@@ -55,6 +104,19 @@ namespace IMSPRO
                 MessageBox.Show("Error in connection ..." + e.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
+        }
+
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            clearEntryForm();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            editeUser editUserFrame = new editeUser();
+            editUserFrame.MdiParent = this.ParentForm;
+            editUserFrame.Show();
         }
     }
 }
